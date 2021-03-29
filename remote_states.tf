80f9e79f2864d3feb598b1aa590cbc74446d8e40
David Redmin <david.redmin@trio.dhs.gov>
2021-03-29 11:49:02 -0400

    Define remote states that are needed

---
+++ remote_states.tf
@@ -0,0 +1,50 @@
+# ------------------------------------------------------------------------------
+# Retrieves state data from a Terraform backend. This allows use of the
+# root-level outputs of one or more Terraform configurations as input data
+# for this configuration.
+# ------------------------------------------------------------------------------
+
+data "terraform_remote_state" "dns_certboto" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-dns-certboto/terraform.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "dns_cyber_dhs_gov" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-dns-cyber.dhs.gov.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "users" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/users.tfstate"
+  }
+
+  workspace = "production"
+}
