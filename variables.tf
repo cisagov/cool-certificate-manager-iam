51ceaf19230a668bf2bdc8dfb81379c47e5b0009
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-04-11 10:45:05 -0400

    Add a required variable for the name of the Terraform state bucket
    This will be used to avoid hard-coding the bucket name in remote_states.tf.

--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
+variable "terraform_state_bucket" {
+  description = "The name of the S3 bucket where Terraform state is stored."
+  nullable    = false
+  type        = string
+}
+
 variable "users" {
   description = "A list containing the usernames of each user that is allowed to manage certificates.  Example: [ \"firstname1.lastname1\", \"firstname2.lastname2\" ]."
   nullable    = false
